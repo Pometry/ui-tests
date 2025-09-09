2d5cd93da3216c00a6892c5499e1b21cb7125ba3
Louis Chan <louisch@fastmail.com>
2025-09-09 18:45:53 +0800

    Use absolute path for volume in docker command

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,8 +9,6 @@ import * as path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '.env'), quiet: true });
 dotenv.config({ path: path.resolve(__dirname, '..', '.env'), quiet: true });
 
-console.log('Using raphtory tag', process.env.RAPHTORY_GRAPHQL_DOCKER_TAG);
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -76,7 +74,7 @@ export default defineConfig({
     webServer: [
         {
             name: 'raphtory-gql',
-            command: `docker run --read-only -v ../applications/vanilla/test_server.py:/var/lib/raphtory/test_server.py -v /tmp/vanilla-graphs:/tmp/vanilla-graphs -p 1736:1736 --entrypoint="python" pometry/raphtory:${process.env.RAPHTORY_GRAPHQL_DOCKER_TAG}-python /var/lib/raphtory/test_server.py`,
+            command: `docker run --read-only -v ${path.resolve(__dirname, '../applications/vanilla/test_server.py')}:/var/lib/raphtory/test_server.py -v /tmp/vanilla-graphs:/tmp/vanilla-graphs -p 1736:1736 --entrypoint="python" pometry/raphtory:${process.env.RAPHTORY_GRAPHQL_DOCKER_TAG}-python /var/lib/raphtory/test_server.py`,
             port: 1736,
             timeout: 120 * 1000,
             stdout: 'pipe',
