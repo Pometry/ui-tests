bdf8a25836fe46bf17acd62868956cc9c1af4e44
Louis Chan <louisch@fastmail.com>
2025-09-10 17:03:03 +0800

    Enable blob reporter on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,10 +20,9 @@ export default defineConfig({
     // forbidOnly: !!process.env.CI,
     // /* Retry on CI only */
     retries: 3,
-    // /* Opt out of parallel tests on CI. */
-    // workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    // We use blob on CI to enable sharding
+    reporter: process.env.CI ? 'blob' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
