8cf6ccc7967f1fa608d985fc93da2c6cae0a4bef
Louis Chan <louisch@fastmail.com>
2025-10-15 15:26:56 +0800

    ESLint Upgrade (#2)
    * Upgrade to ESLint 9
    
    * Upgrade Prettier
    
    * Add husky and lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,9 @@
 const config = {
     '!(**/__generated/**/*)*.{ts,tsx,graphql}': () => ['pnpm run tsc'],
-    '!(**/__generated/**/*)!(**/types/**/*)*.{ts,tsx,graphql,cjs,mjs,js,jsx,json,yaml,yml}':
-        ['pnpm run eslint', 'pnpm run format'],
+    '!(**/__generated/**/*)!(**/types/**/*)*.{ts,tsx,graphql,cjs,mjs,js,jsx}': [
+        'pnpm run eslint',
+        'pnpm run format',
+    ],
     '!(**/__generated/**/*)*.css': (filenames) => [
         `pnpm run format ${filenames}`,
     ],
