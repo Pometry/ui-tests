e8f800cdb4264d3cf8a3955e2e63da939d20a0d5
Louis Chan <louisch@fastmail.com>
2025-11-22 15:52:34 +0800

    Revert change to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,7 @@ export default defineConfig({
     // /* Fail the build on CI if you accidentally left test.only in the source code. */
     // forbidOnly: !!process.env.CI,
     // /* Retry on CI only */
-    retries: 1,
-    workers: 2,
+    retries: 3,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     // We use blob on CI to enable sharding
     reporter: 'html',
